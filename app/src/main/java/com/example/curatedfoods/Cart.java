cb5c38cba4f8d5c6b42c1f3e7d388fd1b0ace8b0
srageh <ragehsuhaib@gmail.com>
2022-12-15 19:10:02 -0500

    cart items showing

--- app/src/main/java/com/example/curatedfoods/Cart.java
+++ app/src/main/java/com/example/curatedfoods/Cart.java
@@ -2,21 +2,61 @@ package com.example.curatedfoods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Cart extends AppCompatActivity {
 
-    TextView cartValuesTV;
+    TextView cartValuesTV, itemQuantityTV, itemNamesTV;
+    String itemName, itemQuantity;
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
         setContentView(R.layout.activity_cart);
+
+
+//
+//
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.grLayout);
+        LinearLayout.LayoutParams test = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
         cartValuesTV = findViewById(R.id.cartValuesTV);
+        itemQuantityTV = findViewById(R.id.itemQuantityTV);
+        itemNamesTV = findViewById(R.id.itemNamesTV);
+
         DBHelper dbHelper = new DBHelper(this);
+        String data = dbHelper.displayItems();
+        String[] arrOfStr = data.split("\n");
+//        int x = 1;
+//        for (int i = 0; i < arrOfStr.length; i++) {
+//            String [] itemRow = arrOfStr[i].split(",");
+////            itemName = itemRow[0];
+////            itemQuantity = itemRow[1];
+//            TextView valueTV = new TextView(this);
+//            valueTV.setText(itemRow[0] + " Price: " + itemRow[1] + " " );
+//            valueTV.setId(x);
+//            valueTV.setLayoutParams(test);
+//            linearLayout.addView(valueTV);
+//
+//
+//            x++;
+//        }
 
-
-        cartValuesTV.setText(dbHelper.displayItems());
+        itemQuantityTV.setText(data);
+        //itemQuantityTV.setText("Name" + arrOfStr[1]);
 
     }
 }
